56bdab252413b8734b8a9a7f872699ec837d4316
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2026-04-29 16:57:36 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MeuApp());
+}
+
+class MeuApp extends StatelessWidget {
+  const MeuApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // O MaterialApp é o ponto de partida obrigatório para apps Material
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 139, 88, 221)),
+        useMaterial3: true,
+      ),
+      home: const PaginaPrincipal(),
+    );
+  }
+}
+
+class PaginaPrincipal extends StatelessWidget {
+  const PaginaPrincipal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // O Scaffold agora está corretamente envolvido pelo MaterialApp
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Não Ligo Mais'),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      drawer: const Drawer(
+        child: Center(child: Text('Se você vai ou se você volta atrás')),
+      ),
+      body: const Center(
+        child: Text('Coração blindado'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'eu ja tô em paz demais'),
+          BottomNavigationBarItem(icon: Icon(Icons.business), label: 'chorei o que tinha agora tanto faz'),
+        ],
+      ),
+    );
+  }
+}
